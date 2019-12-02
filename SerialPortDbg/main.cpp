9cccd28390c5a875f8d8d1be06593ab83d892a90
nereus <fangyuping0521@gmail.com>
2019-12-02 20:44:04 +0800

    set style to Fusion

--- SerialPortDbg/main.cpp
+++ SerialPortDbg/main.cpp
@@ -1,11 +1,15 @@
-#include "mainwidget.h"
+﻿#include "mainwidget.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWidget w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    //qDebug()<<QStyleFactory::keys();
+    /* Available styles: "windowsvista", "Windows", "Fusion" */
+    app.setStyle(QStyleFactory::create("Fusion"));
+    MainWidget widget;
+    widget.show();
+    return app.exec();
 }
