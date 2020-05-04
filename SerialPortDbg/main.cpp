b8ca8c211d59d3e414b7ec0278307fbea2916393
code <fypyndl@163.com>
2020-05-04 19:30:32 +0800

    Enable High Dpi Scaling
    EnableHighDpiScaling

--- SerialPortDbg/main.cpp
+++ SerialPortDbg/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
     //qDebug()<<QStyleFactory::keys();
     /* Available styles: "windowsvista", "Windows", "Fusion" */
